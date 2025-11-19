1fe16de6575a2c2b22f9f1d61c4ee69c3d746345
davidvensilinr <relaxbro2005@gmail.com>
2025-11-19 19:02:24 +0530

    signup decent ui setup

--- frontend/app/pages/signup/page.tsx
+++ frontend/app/pages/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client"
-import Navbar from "@/app/components/Navbar"
 import Link from 'next/link'
+import Navbar from "@/app/components/Navbar"
 export default function Login(){
     const sub =()=>{
         alert("Success");
@@ -8,7 +8,7 @@ export default function Login(){
     return(
         <div>
             <Navbar/>
-            <h1 className="text-center text-5xl">Login</h1>
+            <h1 className="text-center text-5xl">Signup</h1>
             <form onSubmit={sub}>
                 <h1 className="w-full text-3xl">Email :</h1>
                 <input className="w-full p-4 bg-white text-black"
@@ -23,10 +23,17 @@ export default function Login(){
                 placeholder="Enter your password"
                 type="password"
                 />
+                <label className="text-3xl">Confirm your Password :</label>
+                <input
+                className="w-full p-4 bg-white text-black mb-1"
+                required
+                placeholder="Re-Enter your password"
+                type="password"
+                />
                 <button type="submit" className="w-full bg-amber-200 text-black hover:bg-blue-400 p-2 text-2xl">Login</button>
                 <button className="w-full bg-amber-200 text-black hover:bg-blue-400 p-2 text-2xl mt-1">Continue with Google</button>
             </form>
-            <p>Don&apos;t have an account? <Link href="/pages/signup">Signup</Link></p>
+            <p>Already have an account? <Link href="/pages/login">Login</Link></p>
         </div>
     );
 }
