b5d7c009c081597890b35cb7c25058296924dba2
davidvensilinr <relaxbro2005@gmail.com>
2025-11-20 10:09:01 +0530

    flex no wrap for company cards in home page & company card styling

--- frontend/app/components/CompanyCard.tsx
+++ frontend/app/components/CompanyCard.tsx
@@ -2,11 +2,11 @@
 import Image from 'next/image'
 export default function CompanyCard(){
     return(
-        <div>
+        <div className="bg-white rounded-2xl w-1/4 text-black p-4">
             <Image src={'/assets/image.png'} alt="google" width={100} height={100}/>
-            <h3>Google</h3>
-            <p>Salary : 40lpa</p>
-            <button>Apply</button>
+            <h3 className='font-bold'>Google</h3>
+            <p className="font-bold">Salary : 40lpa</p>
+            <button className='bg-amber-200 hover:bg-blue-400 w-full p-4 rounded-2xl mt-3'>Apply</button>
         </div>
     );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,9 +6,14 @@ export default function Home() {
   return (
     <div>
       
-      <h1 className="text-center">Job Search</h1>
+      <h1 className="text-center text-5xl font-bold font-sans p-3">Job Search</h1>
       <Navbar/>
+      <div className="flex flex-nowrap gap-2">
       <CompanyCard/>
+      <CompanyCard/>
+      <CompanyCard/>
+      <CompanyCard/>
+      </div>
     </div>
   );
 }
