5619f018dc6339bf027d85e97b2803eac96711d9
davidvensilinr <relaxbro2005@gmail.com>
2025-11-20 10:01:23 +0530

    company card import in homepage

--- frontend/app/components/CompanyCard.tsx
+++ frontend/app/components/CompanyCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function CompanyCard(){
     return(
         <div>
-            <Image src={'assets/image.png'} alt="google" />
+            <Image src={'/assets/image.png'} alt="google" width={100} height={100}/>
             <h3>Google</h3>
             <p>Salary : 40lpa</p>
             <button>Apply</button>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 import Image from "next/image";
 import Navbar from "./components/Navbar"
+import CompanyCard from "./components/CompanyCard";
 export default function Home() {
   return (
     <div>
       
       <h1 className="text-center">Job Search</h1>
       <Navbar/>
+      <CompanyCard/>
     </div>
   );
 }
