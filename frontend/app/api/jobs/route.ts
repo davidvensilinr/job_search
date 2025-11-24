b3bc83f1a72d3e260c752087bd8228843e6ad28b
davidvensilinr <relaxbro2005@gmail.com>
2025-11-24 11:33:59 +0530

    prisma v5 setup since v7 requires ACCELERATE & connection established and connected

--- frontend/app/api/jobs/route.ts
+++ frontend/app/api/jobs/route.ts
@@ -1,12 +1,12 @@
 import prisma from "@/lib/prisma";
-export default async function GET(){
-try{
-    const jobs=prisma.jobs.findMany();
-    console.log(jobs);
-    return Response.json(jobs);
-}
-catch(error){
-    console.error("Error fetching contents: ",error);
-    return Response.json({error:"failed"},{status:500});
-}
+
+export async function GET() {
+  try {
+    await prisma.$connect();
+    const count = await prisma.jobs.count();
+    return Response.json({ status: "OK", jobsInDB: count });
+  } catch (err) {
+    console.error(err);
+    return Response.json({ error: "Connection failed" }, { status: 500 });
+  }
 }
